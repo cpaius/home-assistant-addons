38694f92b0f8800547af9a5bd7a7bb33091803e3
cpaius <20580928+cpaius@users.noreply.github.com>
2022-04-05 22:32:27 +0100

    Add bin collections addon. Use pip3

--- south-lanarkshire-bin-collection/scripts/run.sh
+++ south-lanarkshire-bin-collection/scripts/run.sh
@@ -7,7 +7,7 @@ pushd collections
 
 REFRESH_INTERVAL_SECONDS=$(bashio::config 'refresh_interval_seconds')
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 echo "sh /collections/execute-get-bin-collections.sh" > /etc/cron.hourly/collections
 chmod +x /etc/cron.hourly/collections
