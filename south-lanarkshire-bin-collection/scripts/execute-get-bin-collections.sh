4030df89721f438e01bba73784fd70031648c474
cpaius <20580928+cpaius@users.noreply.github.com>
2022-04-05 22:43:42 +0100

    Add bin collections addon. Fix script Bump version

--- south-lanarkshire-bin-collection/scripts/execute-get-bin-collections.sh
+++ south-lanarkshire-bin-collection/scripts/execute-get-bin-collections.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-source $HOME/.bash_profile
-source /collections/env.sh
+source /env.sh
 
 mkdir -p ${OUTPUT_FOLDER_NAME}
-python3 /collections/south_lanarkshire_council.py
+python3 /south_lanarkshire_council.py
 
 echo ${OUTPUT_FOLDER_NAME}
 ls -alrt ${OUTPUT_FOLDER_NAME}
