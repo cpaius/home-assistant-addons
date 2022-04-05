d586ed821c33937f3d828b7787f430ed016021f8
cpaius <20580928+cpaius@users.noreply.github.com>
2022-04-05 23:08:53 +0100

    Add bin collections addon. Invoke script on startup

--- south-lanarkshire-bin-collection/scripts/run.sh
+++ south-lanarkshire-bin-collection/scripts/run.sh
@@ -6,11 +6,11 @@ mkdir -p collections
 pushd collections
 
 REFRESH_INTERVAL=$(bashio::config 'refresh_interval')
-echo ${REFRESH_INTERVAL}
+echo "Refresh interval $REFRESH_INTERVAL"
 sh /collections/execute-get-bin-collections.sh
 
-echo "sh /collections/execute-get-bin-collections.sh" > /etc/periodic/${REFRESH_INTERVAL}/collections
-chmod +x /etc/periodic/${REFRESH_INTERVAL}/collections
+echo "sh /collections/execute-get-bin-collections.sh" > /etc/periodic/$REFRESH_INTERVAL/collections
+chmod +x /etc/periodic/$REFRESH_INTERVAL/collections
 
 
 
