2a43ea2f913452ea3339e2d6d542a1e5ae049c00
cpaius <20580928+cpaius@users.noreply.github.com>
2022-04-05 22:29:44 +0100

    Add bin collections addon. Added config. Keep container alive

--- south-lanarkshire-bin-collection/scripts/run.sh
+++ south-lanarkshire-bin-collection/scripts/run.sh
@@ -5,6 +5,8 @@ echo "Running South Lanarkshire Bin Collection addon"
 mkdir -p collections
 pushd collections
 
+REFRESH_INTERVAL_SECONDS=$(bashio::config 'refresh_interval_seconds')
+
 pip install -r requirements.txt
 
 echo "sh /collections/execute-get-bin-collections.sh" > /etc/cron.hourly/collections
