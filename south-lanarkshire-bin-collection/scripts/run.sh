d4c3575e1ae43579e86f91b88ee8c769534ec01a
cpaius <20580928+cpaius@users.noreply.github.com>
2022-04-08 13:00:20 +0100

    Add bin collections addon. Trying to run just hourly cron

--- south-lanarkshire-bin-collection/scripts/run.sh
+++ south-lanarkshire-bin-collection/scripts/run.sh
@@ -3,14 +3,9 @@
 echo "Running South Lanarkshire Bin Collection addon"
 
 mkdir -p collections
-pushd collections
 
 chmod +x /collections/execute-get-bin-collections.sh
-./collections/execute-get-bin-collections.sh
+sh /collections/execute-get-bin-collections.sh
 
 echo "sh /collections/execute-get-bin-collections.sh" > /etc/periodic/hourly/collections
 chmod +x /etc/periodic/hourly/collections
-
-
-
-popd
